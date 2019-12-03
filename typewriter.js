437bd6333388eb12a73e88877abe7b44a10f22a1
Jay Seo <sjs5953@hotmail.com>
2019-12-03 21:47:44 +0000

    finished animated sentence

---
+++ typewriter.js
@@ -0,0 +1,13 @@
+const sentence = "hello there from lighthouse labs";
+let time = 0
+let lastIndex = sentence.length -1
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    if(i === lastIndex){
+      process.stdout.write(sentence[i] + "\n");
+     } else {
+       process.stdout.write(sentence[i]);
+  }
+}, time ) 
+  time += 50
+}
